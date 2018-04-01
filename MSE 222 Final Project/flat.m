4504d156356b77a1b9768e0baae08128ecf11809
Harjaapb <37712706+Harjaapb@users.noreply.github.com>
2018-03-31 17:29:24 -0700

    Update flat.m

--- MSE 222 Final Project/flat.m
+++ MSE 222 Final Project/flat.m
@@ -5,7 +5,8 @@ function [ vf,t,xf,yf ] = flat(vi,theta,l,x0,y0,mu)
   vf = sqrt((0.5*mb*vi^2 + 0.5*Ib*(vi/rb)^2 - mu*mb*g*l*cos(theta) - mb*g*l*sin(theta))/(0.5*mb + 0.5*Ib/rb^2));
   xf = x0 + l*cos(theta);
   yf = y0 + l*sin(theta);
-  t = abs((vf - vi)/g);
+  a = g*sin(theta) - mu*g*cos(theta);
+  t = abs((vf - vi)/a);
 
   %plot the segment on x vs y graph
   x=[x0 xf];
