5dbd85f31adf8f72ca8d4c0f980d9c0565b0365a
Rai Gohalwar <raigohalwar@gmail.com>
2018-04-02 17:50:51 -0700

    Update flat.m
    added plotting, time and comments . I think this function is done

--- MSE 222 Final Project/flat.m
+++ MSE 222 Final Project/flat.m
@@ -1,17 +1,35 @@
+% flat.m is used to analyse the ball through a straight line motion 
+% input of function are vi = initial velocity, theta = angle of the line, l = length of the line
+% x0 and y0 = initial points and mu = coefficient of friction
+% output of function are vf = final velocity , t = time of travel, xf and yf = final points
+
 function [ vf,t,xf,yf ] = flat(vi,theta,l,x0,y0,mu)
 
-  global mb Ib g rb
-
-  vf = sqrt((0.5*mb*vi^2 + 0.5*Ib*(vi/rb)^2 - mu*mb*g*l*cos(theta) - mb*g*l*sin(theta))/(0.5*mb + 0.5*Ib/rb^2));
-  xf = x0 + l*cos(theta);
-  yf = y0 + l*sin(theta);
-  a = g*sin(theta) - mu*g*cos(theta);
-  t = abs((vf - vi)/a);
-
-  %plot the segment on x vs y graph
-  x=[x0 xf];
-  y=[y0 yf];
-  hold on
-  plot(x,y,'b')
-
+    global mb Ib g rb time f1 f2 f3
+    
+    temp = (0.5*mb*vi^2 + 0.5*Ib*(vi/rb)^2 - mu*mb*g*l*cos(theta) - mb*g*l*sin(theta));
+    vf = sqrt(temp/(0.5*mb + 0.5*Ib/rb^2)); % calculating final velocity
+    xf = x0 + l*cos(theta); % calculating final x and y spots 
+    yf = y0 + l*sin(theta);
+    a = g*sin(theta) - mu*g*cos(theta); % calculating the acceleration 
+    t = abs((vf - vi)/a); % time elapsed for the movement
+    
+    
+    % plot figure 1, x vs y graph
+    figure(f1)
+    x=[x0 xf];
+    y=[y0 yf];
+    plot(x,y,'b')
+    
+    % plot x, y, v, a, vs t graph 
+    figure(f2)
+    deltat = [time t];
+    velocity = [vi vf];
+    acceleration = [a a];
+    plot(deltat, x, deltat, y, deltat, velocity, deltat, acceleration);
+    
+    % since the straight elements don't rotate, there is no anglular velocity or acceleration 
+    
+    time = time+t; % adding to the accumulative time 
 end
+
