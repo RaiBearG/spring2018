471323bce125254a905793c85629b279efb5587c
Rai Gohalwar <raigohalwar@gmail.com>
2018-04-04 22:26:21 -0700

    Update spring.m

--- MSE 222 Final Project/spring.m
+++ MSE 222 Final Project/spring.m
@@ -1,8 +1,21 @@
+% spring.m handles the initial launch of the system. The ball is not in contact with the spring
+% before it is launched. 
+%input argument : x = retraction of the spring in meters
+%putput arguments : vf = final velocity of the ball (since horizontal element after this, the velocity in x
 function [vf] = spring(x)
-    global mb
+    
+    global mb time
 
-    k = 440; % K value of spring 
-    t = 0.05; % impact time
-    F = k*x*x;
-    vf = F*t/mb;
+    k = 440; % K value of spring (
+    
+    t = 0.05; % impact time assumption for the entire project 
+    
+    F = k*x*x;% force of the impact
+    
+    vf = F*t/mb; % velocity of the ball after the impact
+    
+    time = time+t;
+    
+    % since this is the initiating point, no plotting needed 
+    
 end
