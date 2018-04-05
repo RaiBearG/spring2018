ec7eb2665473cee687de6a30c8a94f2a0c7b4c36
Rai Gohalwar <raigohalwar@gmail.com>
2018-04-04 21:29:46 -0700

    Update rotation.m

--- MSE 222 Final Project/rotation.m
+++ MSE 222 Final Project/rotation.m
@@ -5,29 +5,33 @@
 % freefalls towards the curved section. 
 % input arguments : w0 = initial anglular velocity, x0 & y0 = initial x and y positions
 % output arguments : vf = final velocity (assumed in -y direction) , xf yf = final x and y positions
-function [vf,t,xf,yf,al] = rotation(w0,x0,y0)
+function [vf,xf,yf] = rotation(w0,x0,y0)
     global mb g Ib rb time f1 f2 f3;
     
     Ibar = 0.0128*mb/3 + 0.0032*mb; % moment of inertia of the rotating element
     Itot = Ibar + Ib + 2*mb*rb^2; % calculate moment of inertia around rotating axis 
     R = sqrt(2*rb^2); % distance of ball CG from rotating axis 
-    itter=1;
-    for i=(pi/2+5*pi/1000)-(pi/1000):-pi/1000:-pi/4 % for loop with step size of -pi/1000
+
+    for i=(3*pi/4)-(pi/1000):-pi/1000:-pi/4 % for loop with step size of -pi/1000
         alpha = R*cos(i)*mb*g/Itot; % calculating alpha from the moment created by the ball
        
+        %compensating for the time bug when i = pi/2
         if i == pi/2
             alpha = 0.0458;
         end
+        
         a = alpha*R + (w0^2)*R; % calclating the acceleration tangential and normal
         Q = [0.5*alpha w0 pi/1000]; % making a column matrix to calculate time 
         Poly = roots(Q); % using the matlab root function to find root 
         
+        t1 = abs(Poly(1));% preparing for the 2 roots for comparison
+        t2 = abs(Poly(2));
 
         % getting rid of the unwanted root 
-        if Poly(1)>0
-            dt = Poly(1);
-        elseif Poly(2)>0
-            dt = Poly(2);
+        if t1>0 && t1<0.05
+            dt = t1;
+        elseif t2>0 && t2<0.05
+            dt = t2;
         else
                 dt=0;
         end
@@ -45,12 +49,10 @@ function [vf,t,xf,yf,al] = rotation(w0,x0,y0)
         y=[y0 yf];
         plot(x,y,'b')
         
+        
+        
         w0 = wf;
         time = time + dt;
-        t(itter) = dt;
-        %w(itter) = wf;
-        al(itter) = alpha;
-        itter = itter+1;
         x0 = xf;
         y0 = yf;
     end
